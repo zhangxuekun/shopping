7a9006153509394fe5db15ed977bc6eacf2673d8
zhangxuekun <zhangxuekun@wtoip.com>
2019-12-16 12:03:37 +0800

    'feat:update_router'

--- src/main/java/com/luryoo/controller/TemplateController.java
+++ src/main/java/com/luryoo/controller/TemplateController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("tepl")
+@RequestMapping("tmpl")
 public class TemplateController {
 
     @RequestMapping("/thymeleaf")
